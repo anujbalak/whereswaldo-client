f4cace9f90c75631f6b323ab169f65a1503f205d
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-06-16 14:11:34 +0530

    Add backend url

--- src/module/queries.js
+++ src/module/queries.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "https://whereswaldo-backend.onrender.com";
 
 export const getAllEasyChracters = async () => {
     try {
